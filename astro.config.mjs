2c036243a4ad7b6c46a153129ac47a63c8a28edc
Jessica Nakamura <jessinaka@Jessicas-MacBook-Pro.local>
2025-04-27 12:03:29 -1000

    nav and hero tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   site: "https://www.yourwebsite.com", // update me!
-  integrations: [
-    sitemap()
-  ]
+  integrations: [sitemap()],
 });
